35a4dfbb65ddab0c1f53849abb56b12b3e7ba19d
Charini Lakshan <charinilakshani95@gmail.com>
2019-06-17 17:55:04 +0530

    change data base

--- src/main/java/com/charini/ccafe/CCafeApplication.java
+++ src/main/java/com/charini/ccafe/CCafeApplication.java
@@ -17,19 +17,19 @@ public class CCafeApplication {
         SpringApplication.run(CCafeApplication.class, args);
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
-
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+////        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+////        final CorsConfiguration config = new CorsConfiguration();
+////        config.setAllowCredentials(true);
+////        config.addAllowedOrigin("*");
+////        config.addAllowedHeader("*");
+////        config.setAllowedOrigins(Collections.singletonList("*"));
+////        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+////
+////        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+////        source.registerCorsConfiguration("/**", config);
+////        return new CorsFilter(source);
+//    }
 
 }
