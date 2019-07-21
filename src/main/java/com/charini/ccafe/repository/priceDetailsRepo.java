f6ff7548f53062ca9704ebf831ed05adbaf4bc8c
Charini Lakshan <charinilakshani95@gmail.com>
2019-07-21 22:29:04 +0530

    change

--- src/main/java/com/charini/ccafe/repository/priceDetailsRepo.java
+++ src/main/java/com/charini/ccafe/repository/priceDetailsRepo.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface priceDetailsRepo extends JpaRepository<priceDetails, Integer> {
 
-    Optional<priceDetails> findByModelAndAndDamageArea(String model, String damageType);
+    Optional<priceDetails> findByModelAndDamageArea(String model, String damageType);
 }
